3c7e1be2c32cd57db81b121a1f6cdc6cd590f014
gsolorza macbookpro <ge.solorzano@gmail.com>
2020-02-20 13:43:59 -0300

    adding code to get the hotname from output on get_config.py

--- Assessment/get_config.py
+++ Assessment/get_config.py
@@ -11,7 +11,12 @@ for device in tqdm(device_list, ascii=True):
         output = connection.send_command("show running-config")
     except:
         continue
-    hostname = connection.find_prompt().strip("#")
+    hostname = str()
+    for line in output.split("\n"):
+        if "hostname" in line:
+            hostname = line.strip("hostname ")
+            break
+    print(hostname)
     try:
         with open(hostname+".log", "w") as config_file:
             config_file.write(output)
