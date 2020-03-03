4ede43f5b45c6398e1471bde5320cee3380518d2
gsolorza macbookpro <ge.solorzano@gmail.com>
2020-03-03 10:22:02 -0300

    some testing over logs

--- Assessment/get_logs.py
+++ Assessment/get_logs.py
@@ -2,6 +2,10 @@
 
 import json
 
+def get_clock(connection):
+    output = connection.send_command("show clock", use_textfsm=True)
+    print(json.dumps(output, indent=2))
+
 def get_logs(connection):
     output = connection.send_command("show logging", use_textfsm=True)
     print(json.dumps(output, indent=2))
--- Assessment/main.py
+++ Assessment/main.py
@@ -7,7 +7,7 @@ from get_interface_rates import get_interface_status, get_interfaces
 from get_hostname import get_hostname
 from get_inventory import get_inventory
 from get_performance import get_cpu, get_memory, get_storage, get_environment
-from get_logs import get_logs
+from get_logs import get_logs, get_clock
 from get_version import get_version
 from tqdm import tqdm
 from pprint import pprint
@@ -17,11 +17,12 @@ device_list = unpack_device_list()
 def connect(device_list):
     for device in tqdm(device_list, ascii=True):
         try:
-            connection = ConnectHandler(**device)
-            
+            connection = ConnectHandler(**device)       
         except:
             print("SOMETHING WENT WRONG CONNECTING TO HOST "+device["host"]+" VIA SSH")
-        get_version(connection)
+        print(connection.host)
+        get_clock(connection)
 
 if __name__ == "__main__":
     connect(device_list)
+
