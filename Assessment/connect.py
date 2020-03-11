e17a61743486cd4183a9d05877b9e9202818a212
gsolorza macbookpro <ge.solorzano@gmail.com>
2020-03-11 19:22:52 -0300

    correct some error handling on connect class

--- Assessment/connect.py
+++ Assessment/connect.py
@@ -25,33 +25,51 @@ class ConnectManager:
                 connection = ConnectHandler(**device)
 
             except AuthenticationException:
-                device["username"] = "cisco"
-                device["password"] = "cisco"
+                device["username"] = "ibustamante"
+                device["password"] = "Salco.2020"
                 connection = ConnectHandler(**device)
 
             except SSHException:
                 try:
+                    print("SSH")
                     telnet_device = {}
                     telnet_device.update(device)
                     telnet_device["device_type"] = "cisco_ios_telnet"
                     connection = ConnectHandler(**telnet_device)
 
                 except ConnectionResetError:
-                    telnet_device["username"] = "cisco"
-                    telnet_device["password"] = "cisco"
-                    connection = ConnectHandler(**telnet_device)    
-                    
+                    print("TELNET PASSWORD ERROR")
+                    telnet_device["username"] = "ibustamante"
+                    telnet_device["password"] = "Salco.2020"
+                    connection = ConnectHandler(**telnet_device)
+
+                except NetMikoAuthenticationException:
+                    print("TELNET PASSWORD ERROR")
+                    telnet_device["username"] = "ibustamante"
+                    telnet_device["password"] = "Salco.2020"
+                    connection = ConnectHandler(**telnet_device)
+
+                except Exception:
+                    print("THERE WAS AN ERROR WITH THE DEVICE:\n--> {} <-- and the error was {}".format(device.host, failure))
+                    continue
+
                 finally:
-                    hostname = connection.base_prompt
-                    dcom = {hostname: []}
-                    for command in commands:
-                        output_list = connection.send_command(command, use_textfsm=textfsm)
-                        cm = command.replace(" ", "_")
-                        dcom[hostname].append({cm: output_list})
-                    commands_output.append(dcom)
+                    try:
+                        hostname = connection.base_prompt
+                        dcom = {hostname: []}
+                        for command in commands:
+                            output_list = connection.send_command(command, use_textfsm=textfsm)
+                            cm = command.replace(" ", "_")
+                            dcom[hostname].append({cm: output_list})
+                        commands_output.append(dcom)
+                    except Exception as failure:
+                        print("THERE WAS AN ERROR TRYING TO CONNECT TO THE DEVICE:\n--> {} <-- and the error was {}".format(device.host, failure))
+                        continue
+
 
             except Exception as failure:
-                print("THERE WAS AN ERROR TRYING TO CONNECT TO THE DEVICE:\n--> {} <--".format(failure))
+                print("THERE WAS AN ERROR TRYING TO CONNECT TO THE DEVICE:\n--> {} <-- and the error was {}".format(device.host, failure))
+                continue
             
             finally:
                 try:
@@ -63,8 +81,8 @@ class ConnectManager:
                         dcom[hostname].append({cm: output_list})
                     commands_output.append(dcom)
                 except Exception as failure:
-                    print("THERE WAS AN ERROR TRYING TO CREATE DEVICE DATA:\n--> {} <--".format(failure))
-                    
+                    print("THERE WAS AN ERROR TRYING TO CONNECT TO THE DEVICE:\n--> {} <-- and the error was {}".format(device.host, failure))
+
         return commands_output
 
 
